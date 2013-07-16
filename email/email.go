72fb74909401820cf2c0d8fae03a5d5d16edf3f0
Paul Chiu <paulchiu@gmail.com>
2013-07-16 18:24:38 +1000

    Refactored message into its own file.

--- email/email.go
+++ email/email.go
@@ -4,8 +4,6 @@ import (
 	"net/mail"
 	"net/smtp"
 	"regexp"
-	"errors"
-	"strings"
 )
 
 const CRLF = "\r\n"
@@ -14,13 +12,6 @@ type Server struct {
 	Username string
 	Password string
 	Host string
-}
-
-type Message struct {
-	From *mail.Address
-	To []*mail.Address
-	subject string
-	body string
 }
 
 // Send plain text email
@@ -38,60 +29,6 @@ func send(server *Server, message *Message, messageContent []byte) error {
 	auth := smtp.PlainAuth("", server.Username, server.Password, stripPort(server.Host))
 	err := smtp.SendMail(server.Host, auth, message.From.Address, addressListToStrings(message.To), messageContent)
 	return err
-}
-
-// Create a new email message
-func NewMessage(from, to, subject, body string) *Message {
-	message, _ := NewMessageDebug(from, to, subject, body)
-	return message
-}
-
-// Create a new email message, return any reasons for failure
-func NewMessageDebug(from, to, subject, body string) (*Message, error) {
-	fromAddress, err := mail.ParseAddress(from)
-	if err != nil {
-		return nil, errors.New("Invalid from address; " + err.Error())
-	}
-
-	toAddresses, err := mail.ParseAddressList(to)
-	if err != nil {
-		return nil, errors.New("Invalid to address(es); " + err.Error())
-	}
-
-	return &Message{fromAddress, toAddresses, subject, body}, nil
-}
-
-// Generate default message header
-func (message *Message) header() string {
-	header := "From: " + message.From.String() + CRLF
-	header += "To: " + strings.Join(addressListToRFCStrings(message.To), ", ") + CRLF
-	header += "Subject: " + message.subject + CRLF
-	return header
-}
-
-// Generate html message header
-func (message *Message) htmlHeader() string {
-	return message.header() + "Content-Type: text/html" + CRLF
-}
-
-// Convert email message to plain format string
-func (message *Message) PlainString() string {
-	return message.header() + CRLF + message.body
-}
-
-// Convert email message to plain format string
-func (message *Message) HTMLString() string {
-	return message.htmlHeader() + CRLF + message.body
-}
-
-// Convert email message to plain format byte array for sending
-func (message *Message) Plain() []byte {
-	return []byte(message.PlainString())
-}
-
-// Convert email message to html format byte array for sending
-func (message *Message) HTML() []byte {
-	return []byte(message.HTMLString())
 }
 
 // Convert an array of RFC 5322 addresses to strings
